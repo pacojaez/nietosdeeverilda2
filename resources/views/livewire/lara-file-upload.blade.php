7166e769d92d7f86d877693055761848f8ac5f6f
Paco Jaez <pacojaez@gmail.com>
2021-08-03 12:05:48 +0200

    added some styles to nav y showposts

--- resources/views/livewire/lara-file-upload.blade.php
+++ resources/views/livewire/lara-file-upload.blade.php
@@ -1,76 +1,93 @@
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-1">
-            {{ __('Upload_Photo') }}
-        </h2>
-    </x-slot>
+<x-slot name="header">
+    <h2 class="mb-1 text-xl font-semibold leading-tight text-gray-800">
+        {{ __('Subir una foto') }}
+    </h2>
+</x-slot>
 <div>
-@if (session()->has('message')) 
+    @if (session()->has('message'))
     <div class="alert alert-success"> {{ session('message') }} </div>
-@endif
-<div class="container mx-auto mt-2 p-2 h-full flex rounded-lg shadow overflow-hidden justify-center">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-1">RELLENA LOS CAMPOS Y GUARDA LA FOTO</h2>
-</div>
-<div class="container mx-auto mt-2 p-2 h-full flex rounded-lg shadow overflow-hidden">
-    <div class="w-full xl:w-1/2 p-8  m-3 p-2">
-        <form wire:submit.prevent="submit" enctype="multipart/form-data"> 
-            <div class="form-group mb-4 mt-6"> 
-                <label for="exampleInputName" class="block text-gray-700 text-sm font-semibold mb-2">Título</label> 
-                <input type="text" class="form-control text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10" id="exampleInputName" placeholder="Lo que se te ocurra" wire:model="title"> 
-                @error('title') 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div>
-            <div class="form-group mb-4 mt-6">      
-                <label for="exampleInputName">Subtitulo</label> 
-                <input type="text" class="form-control text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10" id="exampleInputName" placeholder="Algo que tenga que ver con la foto, o no :)" wire:model="body"> 
-                @error('body') 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div>
-            <div class="form-group mb-4 mt-6">
-                <label for="exampleInputName">Descripción</label>
-                <textarea name="description" rows="10" cols="50" placeholder="Por si te apetece escribir más" wire:model="description" class="text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10"></textarea> 
-                @error('description') 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div>
-            <div class="form-group mb-4 mt-6">      
-                <label for="exampleInputName">Lugar</label> 
-                <input type="text" class="form-control text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10" id="exampleInputName" placeholder="¿Donde estabas?" wire:model="place"> 
-                @error('place') 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div>
-            <div class="form-group mb-4 mt-6">      
-                <label for="exampleInputName">Fecha aproximada</label> 
-                <input type="text" class="form-control text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10" id="exampleInputName" placeholder="Solo el año, ejem: 1982" wire:model="date"> 
-                @error('date') 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div>
-            
-            <div class="form-group mb-4 mt-6"> 
-                <label for="exampleInputName">File</label><span wire:dirty wire:target="file_name">Processing...</span> 
-                
-                <input type="file" class="form-control text-sm appearance-none rounded w-full py-2 px-3 text-gray-700 bg-gray-200 leading-tight focus:outline-none focus:shadow-outline h-10" id="exampleInputName" wire:model="file_name"> 
-                @error('name')
-                 
-                <span class="text-danger">{{ $message }}</span> 
-                @enderror 
-            </div> 
-            <button type="submit" class="w-full bg-gray-800 hover:bg-grey-900 text-white text-sm py-2 px-4 font-semibold rounded focus:outline-none focus:shadow-outline h-10">Save</button>
-        </form>   
+    @endif
+    <div class="container flex justify-center h-full p-2 mx-auto mt-2 overflow-hidden rounded-lg shadow">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">RELLENA LOS CAMPOS Y GUARDA LA FOTO</h2>
     </div>
-    <div class="w-full xl:w-1/2 p-8 m-3 p-2">
-        Photo Preview:     
-            <div wire:dirty wire:target="file_name" class="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
-                <span class="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0" style="top: 50%;">
-                  <i class="fas fa-circle-notch fa-spin fa-5x"></i>
+    <div class="container flex flex-col h-full p-2 mx-auto overflow-hidden rounded-lg shadow md:flex-row">
+        <div class="w-full p-2 p-8 m-3 xl:w-1/2">
+            <form wire:submit.prevent="submit" enctype="multipart/form-data">
+                @csrf
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName" class="block mb-2 text-sm font-semibold text-gray-700">Título</label>
+                    <input type="text"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none form-control focus:outline-none focus:shadow-outline"
+                        id="exampleInputName" placeholder="Lo que se te ocurra" wire:model="title">
+                    @error('title')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName">Subtitulo</label>
+                    <input type="text"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none form-control focus:outline-none focus:shadow-outline"
+                        id="exampleInputName" placeholder="Algo que tenga que ver con la foto, o no :)"
+                        wire:model="body">
+                    @error('body')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName">Descripción</label>
+                    <textarea name="description" rows="10" cols="50" placeholder="Por si te apetece escribir más"
+                        wire:model="description"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none focus:outline-none focus:shadow-outline"></textarea>
+                    @error('description')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName">Lugar</label>
+                    <input type="text"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none form-control focus:outline-none focus:shadow-outline"
+                        id="exampleInputName" placeholder="¿Donde estabas?" wire:model="place">
+                    @error('place')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName">Fecha aproximada</label>
+                    <input type="text"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none form-control focus:outline-none focus:shadow-outline"
+                        id="exampleInputName" placeholder="Solo el año, ejem: 1982" wire:model="date">
+                    @error('date')
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="mt-6 mb-4 form-group">
+                    <label for="exampleInputName">File</label><span wire:dirty
+                        wire:target="file_name">Processing...</span>
+
+                    <input type="file"
+                        class="w-full h-10 px-3 py-2 text-sm leading-tight text-gray-700 bg-gray-200 rounded appearance-none form-control focus:outline-none focus:shadow-outline"
+                        id="exampleInputName" wire:model="file_name">
+                    @error('name')
+
+                    <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+                <button type="submit"
+                    class="w-full h-10 px-4 py-2 text-sm font-semibold text-white bg-gray-800 rounded hover:bg-grey-900 focus:outline-none focus:shadow-outline">Guardar</button>
+            </form>
+        </div>
+        <div class="w-full p-2 p-8 m-3 xl:w-1/2">
+            Photo Preview:
+            <div wire:dirty wire:target="file_name"
+                class="fixed top-0 left-0 z-50 block w-full h-full bg-white opacity-75">
+                <span class="relative block w-0 h-0 mx-auto my-0 text-green-500 opacity-75 top-1/2" style="top: 50%;">
+                    <i class="fas fa-circle-notch fa-spin fa-5x"></i>
                 </span>
-            </div>      
-        @if ($file_name)
+            </div>
+            @if ($file_name)
             <img src="{{ $file_name->temporaryUrl() }}">
-        @endif
+            @endif
+        </div>
     </div>
-</div>
 </div>
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,86 +1,86 @@
 <div class="pb-2 m-4">
-    <div class="w-full flex pb-2 m-4 justify-center">
-        <div class="w-2/6 mx-1">
-            <input wire:model.debounce.500ms="search" type="search" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Buscar foto o personas...">
+    <div class="flex flex-col justify-center w-full pb-2 m-4 md:flex-row">
+        <div class="relative m-2">
+            <input wire:model.debounce.500ms="search" type="search" class="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Buscar foto o personas...">
         </div>
-        <div class="w-1/6 relative mx-1">
-            <select wire:model="orderBy" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+        <div class="relative m-2">
+            <select wire:model="orderBy" class="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                 <option value="id">Orden de Subida</option>
                 <option value="title">Titulo</option>
                 <option value="file_name">Nombre de archivo</option>
                 <option value="created_at">Fecha de Subida</option>
             </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                {{-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                {{-- <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
             </div>
         </div>
-        <div class="w-1/6 relative mx-1">
-            <select wire:model="orderAsc" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+        <div class="relative m-2">
+            <select wire:model="orderAsc" class="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                 <option value="1">Ascending</option>
                 <option value="0">Descending</option>
             </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                {{-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                {{-- <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
             </div>
         </div>
-        <div class="w-1/6 relative mx-1">
-            <select wire:model="perPage" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+        <div class="relative m-2">
+            <select wire:model="perPage" class="block w-full px-4 py-3 pr-8 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
                 <option value="10">10</option>
                 <option value="25">25</option>
                 <option value="50">50</option>
                 <option value="100">100</option>
             </select>
-            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                {{-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
+            <div class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+                {{-- <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg> --}}
             </div>
         </div>
-        {{-- <div class="w-1/6 relative mx-1 flex justify-center items-center">
-            <div x-data="range()" x-init="mintrigger(); maxtrigger()" class="relative max-w-xl w-full">
+        {{-- <div class="relative flex items-center justify-center w-1/6 mx-1">
+            <div x-data="range()" x-init="mintrigger(); maxtrigger()" class="relative w-full max-w-xl">
               <div>
                 <input type="range"
                        step="1"
                        x-bind:min="min" x-bind:max="max"
                        x-on:input="mintrigger"
                        x-model="minprice"
-                       class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
-          
-                <input type="range" 
+                       class="absolute z-20 w-full h-2 opacity-0 appearance-none cursor-pointer pointer-events-none">
+
+                <input type="range"
                        step="1"
                        x-bind:min="min" x-bind:max="max"
                        x-on:input="maxtrigger"
                        x-model="maxprice"
-                       class="absolute pointer-events-none appearance-none z-20 h-2 w-full opacity-0 cursor-pointer">
-          
+                       class="absolute z-20 w-full h-2 opacity-0 appearance-none cursor-pointer pointer-events-none">
+
                 <div class="relative z-10 h-2">
-          
-                  <div class="absolute z-10 left-0 right-0 bottom-0 top-0 rounded-md bg-gray-200"></div>
-          
-                  <div class="absolute z-20 top-0 bottom-0 rounded-md bg-blue-300" x-bind:style="'right:'+maxthumb+'%; left:'+minthumb+'%'"></div>
-          
-                  <div class="absolute z-30 w-6 h-6 top-0 left-0 bg-blue-300 rounded-full -mt-2 -ml-1" x-bind:style="'left: '+minthumb+'%'"></div>
-          
-                  <div class="absolute z-30 w-6 h-6 top-0 right-0 bg-blue-300 rounded-full -mt-2 -mr-3" x-bind:style="'right: '+maxthumb+'%'"></div>
-           
+
+                  <div class="absolute top-0 bottom-0 left-0 right-0 z-10 bg-gray-200 rounded-md"></div>
+
+                  <div class="absolute top-0 bottom-0 z-20 bg-blue-300 rounded-md" x-bind:style="'right:'+maxthumb+'%; left:'+minthumb+'%'"></div>
+
+                  <div class="absolute top-0 left-0 z-30 w-6 h-6 -mt-2 -ml-1 bg-blue-300 rounded-full" x-bind:style="'left: '+minthumb+'%'"></div>
+
+                  <div class="absolute top-0 right-0 z-30 w-6 h-6 -mt-2 -mr-3 bg-blue-300 rounded-full" x-bind:style="'right: '+maxthumb+'%'"></div>
+
                 </div>
-          
+
               </div>
-              
-              <div class="flex justify-between items-center py-5">
+
+              <div class="flex items-center justify-between py-5">
                 <div>
-                  <input type="text" maxlength="4" x-on:input="mintrigger" x-model="minprice" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
+                  <input type="text" maxlength="4" x-on:input="mintrigger" x-model="minprice" class="w-24 px-3 py-2 text-center border border-gray-200 rounded">
                 </div>
                 <div>
-                  <input type="text" maxlength="4" x-on:input="maxtrigger" x-model="maxprice" class="px-3 py-2 border border-gray-200 rounded w-24 text-center">
+                  <input type="text" maxlength="4" x-on:input="maxtrigger" x-model="maxprice" class="w-24 px-3 py-2 text-center border border-gray-200 rounded">
                 </div>
               </div>
-              
+
             </div>
         </div> --}}
     </div>
-    {{-- <div class="w-full flex pb-10">
+    {{-- <div class="flex w-full pb-10">
         <button
-        class="py-2 px-4 bg-transparent text-red-600 font-semibold border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0"
-      >Order Now</button> 
+        class="px-4 py-2 font-semibold text-red-600 transition duration-200 ease-in transform bg-transparent border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent hover:-translate-y-1 active:translate-y-0"
+      >Order Now</button>
     </div>--}}
     {{-- @livewire('decades-component') --}}
     @if($noImages)
@@ -88,60 +88,63 @@
     @else
     {{-- <div class="flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">                      
+            <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <div class="overflow-hidden border-b border-gray-200 shadow sm:rounded-lg">
                             @foreach($posts as $post)
                                 <div>
-                                    <h2 class="border px-4 py-2">{{ $post->id }}</h2>
-                                    <h3 class="border px-4 py-2">{{ $post->title }}</h3>
-                                    {{-- <td class="border px-4 py-2">{{ $post->body }}</td> --}}
+                                    <h2 class="px-4 py-2 border">{{ $post->id }}</h2>
+                                    <h3 class="px-4 py-2 border">{{ $post->title }}</h3>
+                                    {{-- <td class="px-4 py-2 border">{{ $post->body }}</td> --}}
                                     {{--<img src="\storage\public\{{ $post->file_name}}" />
-                                    <div class="border px-4 py-2">
-                                    <button wire:click="edit({{ $post->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
-                                    <button wire:click="delete({{ $post->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                    <div class="px-4 py-2 border">
+                                    <button wire:click="edit({{ $post->id }})" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Edit</button>
+                                    <button wire:click="delete({{ $post->id }})" class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700">Delete</button>
                                     </div>
                                 </div>
                             @endforeach
-                            {!! $posts->links() !!}                   
+                            {!! $posts->links() !!}
                 </div>
             </div>
         </div>
     </div> --}}
-    <div class="container my-12 mx-auto px-4 md:px-12">
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+    <div class="container px-4 mx-auto my-12 md:px-12">
+        <hr>
+        {!! $posts->links() !!}
+        <hr>
+        <div class="flex flex-wrap mt-2 mb-2 -mx-1 lg:-mx-4">
             @foreach($posts as $post)
             <!-- Column -->
-            <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-    
+            <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+
                 <!-- Article -->
                 <article class="overflow-hidden rounded-lg shadow-lg">
                     <a href="{{ route('post', $post->id) }}">
-                        
-                        <img alt="{{ $post->title }}" class="block h-auto w-full" src="{{asset('storage/images/posts/'.$post->file_name)}}">
+
+                        <img alt="{{ $post->title }}" class="block w-full h-auto" src="{{asset('storage/images/posts/'.$post->file_name)}}">
                     </a>
-                    <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                    <header class="flex items-center justify-between p-2 leading-tight md:p-4">
                         <h1 class="text-lg">
-                            <a class="no-underline hover:underline text-black font-bold" href="{{ route('post', $post->id) }}">
+                            <a class="font-bold text-black no-underline hover:underline" href="{{ route('post', $post->id) }}">
                                {{ $post->title }}
                             </a>
                         </h1>
                     </header>
                     <body>
-                        <h2 class="text-lg text-grey-darker text-sm">
+                        <h2 class="text-sm text-lg text-grey-darker">
                             {{ $post->body }}
                         </h2>
-                        <h3 class="text-grey-darker text-sm">
+                        <h3 class="text-sm text-grey-darker">
                             {{ $post->description}}
                         </h3>
-                        <h3 class="text-grey-darker text-sm">
+                        <h3 class="text-sm text-grey-darker">
                             Estabamos en <span class="font-bold">{{ $post->place}}</span>
                         </h3>
-                        <h3 class="text-grey-darker text-sm">
+                        <h3 class="text-sm text-grey-darker">
                             En <span class="font-bold">{{ $post->date}}</span>
-                        </h3>          
+                        </h3>
                     </body>
-                    <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                        {{-- <a class="flex items-center no-underline hover:underline text-black" href="#"> --}}
+                    <footer class="flex items-center justify-between p-2 leading-none md:p-4">
+                        {{-- <a class="flex items-center text-black no-underline hover:underline" href="#"> --}}
                             {{-- <img alt="{{ $post->title }}" class="block rounded-full" src="https://picsum.photos/32/32/?random"> --}}
                             <p class="ml-2 text-sm">
                                 {{ $post->user->name }}
@@ -151,16 +154,16 @@
                             <span class="hidden">Like</span>
                             <i class="fa fa-heart"></i>
                         {{-- </a> --}}
-                        <p class="text-grey-darker text-sm">
+                        <p class="text-sm text-grey-darker">
                             Subida el <span class="font-bold">{{ $post->created_at}}</span>
                         </p>
-                    </footer>  
+                    </footer>
                 </article>
                 <!-- END Article -->
-    
+
             </div>
             <!-- END Column -->
-            @endforeach        
+            @endforeach
         </div>
         <hr>
         {!! $posts->links() !!}
